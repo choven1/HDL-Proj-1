33c703c07178b488c5207d9e14811d1b958153aa
choven1 <31868903+choven1@users.noreply.github.com>
2018-10-13 12:54:23 -0600

    Modify to match alternate solution

--- SimpleSend.v
+++ SimpleSend.v
@@ -8,19 +8,17 @@ module SimpleSend(dataOut,Go,clk,reset,Ready2Go);
 	input	Go, clk, reset;
 
 	parameter   [3:1]  NumLEDs = 3'b101;
-	wire		shipGRB, Done, allDone, Run, Flag;
-	wire 		[1:0] qmode;
-	wire		LoadGRBPattern, ShiftPattern, StartCoding, ClrCounter, IncCounter, theBit, bdone, Cycle, Refresh;
-	wire 		[7:0] Count;
-	wire 		[119:0] GRBSeq, GRBout;
-	wire 		[2:0] Lvl;
-	
+	wire		shipGRB, Done, allDone, Win;
+	wire [1:0]	qmode;
+	wire		LoadGRBPattern, ShiftPattern, StartCoding, ClrCounter, IncCounter, theBit, bdone, Cycle;
+	wire [7:0]	Count;
+	wire [119:0] GRBSeq;
+
 	SSStateMachine	sssm(shipGRB,Done,Cycle,clk,reset,allDone,Ready2Go);
 	GRBStateMachine grbsm(qmode,Done,LoadGRBPattern,ShiftPattern,StartCoding,ClrCounter,IncCounter,
                               shipGRB,theBit,bdone,Count,NumLEDs,clk,reset,allDone);
 	ShiftRegister   shftr(theBit,GRBSeq,LoadGRBPattern,ShiftPattern,clk,reset);
 	BitCounter	btcnt(Count,ClrCounter,IncCounter,clk,reset);
 	NZRbitGEN	nzrgn(dataOut,bdone,qmode,StartCoding,clk,reset);
-	GameEngine1 game1(GRBout,Refresh,Flag,Go,clk,reset,Run,Lvl);
-	GameEngine2 game2(GRBSeq,Cycle,Run,Lvl,Go,clk,reset,GRBout,Refresh,Flag);
+	GameEngine game(GRBSeq,Cycle,Win,Go,clk,reset);
 endmodule
